f3adac4d11beb5a8b70b692da816db7f2b81787d
Emir Fahimi <emirfahimi0@gmail.com>
2023-05-16 16:38:40 +0800

    updated directory contextStore

---
+++ src/contextStore/watchlist-context/WatchlistContext.tsx
@@ -0,0 +1,38 @@
+import { createContext, useState } from "react";
+import { fetchWatchlist } from "../../components/features/handleFunctions";
+
+export interface IInitialState {
+  getWatchlistData: () => void;
+  watchlistState: TMovieType[];
+}
+interface IWatchlistProviderProps {
+  // define props here
+  children?: React.ReactNode;
+}
+
+const initialState: IInitialState = {
+  watchlistState: [],
+  getWatchlistData: () => Promise<void>,
+};
+
+export const WatchlistContext = createContext<IInitialState>(initialState);
+
+export const WatchlistProvider = (props: React.PropsWithChildren<IWatchlistProviderProps>) => {
+  const [state, setState] = useState(initialState);
+
+  const getWatchlistData = async (): Promise<void> => {
+    const responseWatchlist = await fetchWatchlist();
+    if (responseWatchlist !== undefined) {
+      setState({ ...state, watchlistState: responseWatchlist });
+    }
+  };
+  return (
+    <WatchlistContext.Provider
+      value={{
+        watchlistState: state.watchlistState,
+        getWatchlistData,
+      }}>
+      {props.children}
+    </WatchlistContext.Provider>
+  );
+};
